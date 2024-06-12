7c91f8eb21c3bb269684ee36217c456012d4c572
adnanabdalrahman <adnan.24150@gmail.com>
2024-06-12 15:15:27 +0200

    optimise

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
-import {Pokemon} from './pokemonApi.js';
 fetchAllPokemons();
-
-
+console.log('sdsd')
 
 function fetchAllPokemons(limit = 6) {
     let url = "https://pokeapi.co/api/v2/pokemon?limit=" + limit;
     fetch(url)  
     .then(response => response.json())  
-    .then(allpokemons => showAllPokemons(allpokemons))
+    .then(allpokemons => showAllPokemons(allpokemons.results))
     .catch((err) => 
         console.log(`Error: ${err}`)
     );
 }
 
+function fetchOnePokemons(url) {
+    fetch(url)  
+    .then(response => response.json())  
+    .then(pokemon => setPokemonToList(pokemon))
+    .catch((err) => 
+        console.log(`Error: ${err}`)
+    );
+}
 
 function showAllPokemons(allpokemons){
     console.log(allpokemons);
+    allpokemons.forEach(pokemon => {
+        fetchOnePokemons(pokemon.url)
+    });
+}
 
+
+function setPokemonToList(pokemon){
+    console.log(pokemon.height)
 }
 
 
